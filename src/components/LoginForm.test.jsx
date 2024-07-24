960c945cb237e78348d11a6847c53675754d0518
Rizky Rahman Salam <clashikik@gmail.com>
2024-07-24 16:48:47 +0700

    feat: add standard bugs highlighting

--- src/components/LoginForm.test.jsx
+++ src/components/LoginForm.test.jsx
@@ -6,73 +6,73 @@
  *   - should handle password typing correctly
  *   - should call login function when login button is clicked
  */
-import { cleanup, render, screen } from "@testing-library/react";
-import { describe, it, expect, vi, afterEach } from 'vitest';
-import { MemoryRouter } from 'react-router-dom';
-import LoginForm from "./LoginForm";
-import userEvent from "@testing-library/user-event";
-import matchers from "@testing-library/jest-dom/matchers";
+import { cleanup, render, screen } from '@testing-library/react'
+import { describe, it, expect, vi, afterEach } from 'vitest'
+import { MemoryRouter } from 'react-router-dom'
+import LoginForm from './LoginForm'
+import userEvent from '@testing-library/user-event'
+import matchers from '@testing-library/jest-dom/matchers'
 
-expect.extend(matchers);
+expect.extend(matchers)
 
 describe('LoginInput component', () => {
   afterEach(() => {
-    cleanup();
-  });
+    cleanup()
+  })
 
   it('should handle email typing correctly', async () => {
     render(
       <MemoryRouter>
         <LoginForm login={() => {}} />
       </MemoryRouter>
-    );
+    )
     // arrange
-    const emailInput = await screen.getByLabelText('Email');
+    const emailInput = await screen.getByLabelText('Email')
 
     // action
-    await userEvent.type(emailInput, 'emailtest');
+    await userEvent.type(emailInput, 'emailtest')
 
     // assert
-    expect(emailInput).toHaveValue('emailtest');
-  });
+    expect(emailInput).toHaveValue('emailtest')
+  })
 
   it('should handle password typing correctly', async () => {
     render(
       <MemoryRouter>
         <LoginForm login={() => {}} />
       </MemoryRouter>
-    );
+    )
     // arrange
-    const passwordInput = await screen.getByPlaceholderText('enter your password');
+    const passwordInput = await screen.getByPlaceholderText('enter your password')
 
     // action
-    await userEvent.type(passwordInput, 'passwordtest');
+    await userEvent.type(passwordInput, 'passwordtest')
 
     // assert
-    expect(passwordInput).toHaveValue('passwordtest');
-  });
+    expect(passwordInput).toHaveValue('passwordtest')
+  })
 
   it('should call login function when login button is clicked', async () => {
     // arrange
-    const mockLogin = vi.fn();
+    const mockLogin = vi.fn()
     render(
       <MemoryRouter>
         <LoginForm login={mockLogin} />
       </MemoryRouter>
-    );
-    const emailInput = await screen.getByLabelText('Email');
-    await userEvent.type(emailInput, 'emailtest');
-    const passwordInput = await screen.getByLabelText('password-login');
-    await userEvent.type(passwordInput, 'passwordtest');
-    const loginButton = screen.getByRole('button', { name: 'Login' });
+    )
+    const emailInput = await screen.getByLabelText('Email')
+    await userEvent.type(emailInput, 'emailtest')
+    const passwordInput = await screen.getByLabelText('password-login')
+    await userEvent.type(passwordInput, 'passwordtest')
+    const loginButton = screen.getByRole('button', { name: 'Login' })
 
     // action
-    await userEvent.click(loginButton);
+    await userEvent.click(loginButton)
 
     // assert
     expect(mockLogin).toHaveBeenCalledWith({
       email: 'emailtest',
       password: 'passwordtest'
-    });
-  });
-});
+    })
+  })
+})
